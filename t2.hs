0b6e97eac8593ebaf68411941c1ee8de0b67b2ef
nbatista <nbatista@inf.ufsm.br>
2015-03-28 15:10:15 -0300

    add questão 2 - addSuffix

--- t2.hs
+++ t2.hs
@@ -2,3 +2,9 @@
 -- questão 1
 geraPotencias :: Int -> [Int]
 geraPotencias x = [2^x | x <- [x, x-1 .. 0]]
+
+
+
+-- questão 2
+addSuffix :: String -> [String] -> [String]
+addSuffix x y = [y ++ x | y <- y]
